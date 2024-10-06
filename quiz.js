a6d6e5253ef525c775af0c673cf52494b2d2f087
alxsoftware2024 <ashenafin2024@gmail.com>
2024-10-05 22:18:08 -0700

    Home page

---
+++ quiz.js
@@ -0,0 +1,27 @@
+const byname=document.querySelectorAll("input[name='quiz']");
+//console.log(byname);
+//const userAnswer=document.querySelector("input[name='quiz']:checked").value;
+//console.log(userAnswer);
+const feedback=document.getElementById('feedback');
+const submitanswer=document.getElementById('submit-answer');
+function checkAnswer() {
+    const correctAnswer='4';
+    var ele = document.getElementsByName('quiz');
+
+    for(i = 0; i < ele.length; i++) {
+      const  userAnswer=ele[i].value;
+       if(ele[i].checked)
+       {
+        if(correctAnswer==userAnswer)
+        {
+            feedback.textContent="Correct! Well done.";
+        }
+        if(userAnswer!=correctAnswer)
+        {
+            feedback.textContent="That's incorrect. Try again!";
+        }
+       }
+    }
+  
+}
+submitanswer.addEventListener('click',checkAnswer);
